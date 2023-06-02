d55b74d2d9c9759a5cf5855e99ca8475d23be77b
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-06-01 20:07:56 -0500

    Complete mild assignment

--- code_challenges/mild.rb
+++ code_challenges/mild.rb
@@ -2,12 +2,10 @@
 
 # Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
 
-age = 30
+age = 29
 
-def age = 30
-    if age.odd
+    if age.odd? == true
         puts "odd"
-    else age.even
+    else 
         puts "even"
     end
-end
