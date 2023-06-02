47e4646036a90e360a9e7dc28177ec27d50bea2c
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-06-01 19:52:21 -0500

    Complete the mild assignment

--- code_challenges/mild.rb
+++ code_challenges/mild.rb
@@ -1,3 +1,13 @@
 ## Mild
 
-Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+# Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+age = 30
+
+def age = 30
+    if age.odd
+        puts "odd"
+    else age.even
+        puts "even"
+    end
+end
